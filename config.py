894d757b2d456f548c295b5020b1f96dc7cf46b3
XAndreyS <archer.wipt@gmail.com>
2022-02-02 22:20:47 +0500

    version 1

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+
+TOKEN = '5156937134:AAFvveMo7eB3_-_mqK8SzqoSqWmi87kTwnM'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
